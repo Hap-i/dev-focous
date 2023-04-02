fcf5a9660c6c64a4424b2f14899074435f76b681
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2023-04-02 18:44:44 +0530

    audio player added

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,17 +1,44 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import {
+  AiFillPlayCircle,
+  AiFillPauseCircle,
+  AiOutlineStepForward,
+  AiFillStepBackward,
+} from "react-icons/ai";
 
 function AudioPlayer() {
+  const [isPlaying, setisPlaying] = useState(false);
   const audioRef = useRef();
   function playpause() {
-    audioRef.current.play();
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setisPlaying(!isPlaying);
   }
   return (
     <div className="relative z-10">
       {/* <div className="relative z-10 text-white">AudioPlayer</div>; */}
-      <audio src="assets/bg-audio/lofi1.mp3" ref={audioRef}></audio>
-      <button className="bg-black/30 text-white" onClick={playpause}>
-        Play
-      </button>
+      <audio
+        src="assets/bg-audio/lofi1.mp3"
+        preload="none"
+        ref={audioRef}></audio>
+      <div className="bg-black/40 w-fit space-x-5 px-2 py-1 rounded-lg">
+        <button>
+          <AiFillStepBackward size={23} color="white"></AiFillStepBackward>
+        </button>
+        <button onClick={playpause}>
+          {isPlaying ? (
+            <AiFillPauseCircle size={23} color="white"></AiFillPauseCircle>
+          ) : (
+            <AiFillPlayCircle size={23} color="white"></AiFillPlayCircle>
+          )}
+        </button>
+        <button>
+          <AiOutlineStepForward size={23} color="white"></AiOutlineStepForward>
+        </button>
+      </div>
     </div>
   );
 }
